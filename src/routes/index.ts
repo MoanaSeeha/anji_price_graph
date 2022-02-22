51c3611698d2ca0b26043e217669b007103f9044
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-02-23 02:15:43 +0800

    Fixing some issues

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import axios from "axios";
 import { stringify } from "querystring";
 import { Console } from "console";
-import Moralis from "moralis/types";
 
 export const register = (app: express.Application) => {
     // home page
@@ -17,13 +16,6 @@ export const register = (app: express.Application) => {
 
     // get coingecko data
     app.post("/get_data",(req: any, res) => {
-        const {days, pairAddress, chain} = req.body;
-        
-        Moralis.Web3API.defi.getPairReserves({
-            pair_address: pairAddress,
-            to_date: to_date,
-            chain: chain
-        });
         axios
         .get(`https://api.coingecko.com/api/v3/coins/binance-smart-chain/contract/0xfc619ffcc0e0f30427bf938f9a1b2bfae15bdf84/market_chart/?vs_currency=bnb&days=${req.body.days}`)
         .then((response) => {
